52412d94dc79e23cfaa64cbe08bb4d431ee97454
sohhhyeeun <syeankm12076@gmail.com>
2023-03-14 11:22:03 +0900

    pleasure

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -13,4 +13,20 @@ public class HomeController {
     public String showMain() {
         return "안녕하세요.";
     }
+
+    @GetMapping("/home/main2")
+    //개발자가 스프링부트에게 말함.(이런 요청이 오면 아래 메서드를 실행)
+    @ResponseBody
+    //아래 메서드를 실행한 후 그 리턴값을 응답으로 삼으라고 말함.
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    //개발자가 스프링부트에게 말함.(이런 요청이 오면 아래 메서드를 실행)
+    @ResponseBody
+    //아래 메서드를 실행한 후 그 리턴값을 응답으로 삼으라고 말함.
+    public String showMain3() {
+        return "즐거웠습니다.";
+    }
 }

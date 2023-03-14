4e1d6fa322b383c773742aefadca07aa3b8f7182
sohhhyeeun <syeankm12076@gmail.com>
2023-03-14 11:29:04 +0900

    increase

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -29,4 +29,14 @@ public class HomeController {
     public String showMain3() {
         return "즐거웠습니다.";
     }
+
+    int num = 0;
+    @GetMapping("/home/increase")
+    //개발자가 스프링부트에게 말함.(이런 요청이 오면 아래 메서드를 실행)
+    @ResponseBody
+    //아래 메서드를 실행한 후 그 리턴값을 응답으로 삼으라고 말함.
+    public int showIncrease() {
+        num++;
+        return num;
+    }
 }

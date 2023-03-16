4642eef417e1d1171efc1d3fe366ad7aa37c6a9a
sohhhyeeun <syeankm12076@gmail.com>
2023-03-16 14:34:00 +0900

    ãAdd html Login

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -4,70 +4,66 @@ import com.ll.basic1.base.rq.Rq;
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.member.entity.Member;
 import com.ll.basic1.boundedContext.member.service.MemberService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.annotation.RequestScope;
 
 @Controller
+@AllArgsConstructor
 public class MemberController {
     private final MemberService memberService;
     private final Rq rq;
 
-    // 생성자 주입
-    @Autowired
-    public MemberController(MemberService memberService, Rq rq) {
-        this.memberService = memberService;
-        this.rq = rq;
+    @GetMapping("/member/showLogin")
+    public String showLogin() {
+        return "usr/member/login";
     }
 
-    @GetMapping("/member/login")
+    @GetMapping("/member/doLogin")
     @ResponseBody
-
-    public RsData login(String username, String password) {
+    public RsData doLogin(String username, String password) {
         if (username == null || username.trim().length() == 0) {
             return RsData.of("F-3", "username(을)를 입력해주세요.");
         }
         if (password == null || password.trim().length() == 0) {
             return RsData.of("F-4", "password(을)를 입력해주세요.");
         }
-
         RsData rsData = memberService.tryLogin(username, password);
-
         if (rsData.isSuccess()) {
             Member member = (Member) rsData.getData();
-            rq.setCookie("loginedMemberId", member.getId());
+            rq.setSession("loginedMemberId", member.getId());
         }
-
         return rsData;
     }
 
     @GetMapping("/member/logout")
     @ResponseBody
     public RsData logout() {
-        boolean cookieRemoved = rq.removeCookie("loginedMemberId");
+        boolean cookieRemoved = rq.removeSession("loginedMemberId");
 
         if (cookieRemoved == false) {
             return RsData.of("S-2", "이미 로그아웃 상태입니다.");
         }
-
         return RsData.of("S-1", "로그아웃 되었습니다.");
     }
 
     @GetMapping("/member/me")
     @ResponseBody
     public RsData showMe() {
-        long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);
+        long loginedMemberId = rq.getSessionAsLong("loginedMemberId", 0);
 
         boolean isLogined = loginedMemberId > 0;
-
         if (isLogined == false)
             return RsData.of("F-1", "로그인 후 이용해주세요.");
-
         Member member = memberService.findById(loginedMemberId);
-
         return RsData.of("S-1", "당신의 username(은)는 %s 입니다.".formatted(member.getUsername()));
+    }
+
+    @GetMapping("/member/session")
+    @ResponseBody
+    public String showSession() {
+        return rq.getSessionDebugContents().replaceAll("\n", "<br>");
     }
 }
--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -6,6 +6,8 @@ import com.ll.basic1.boundedContext.member.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+// @Component : 아래 클래스의 객체는 Ioc 컨테이너에 의해 생사소멸 관리된다.
+// @Service : @Component 와 같은 의미, 가독성 때문에 이렇게 표기
 @Service
 @AllArgsConstructor
 public class MemberService {
@@ -22,7 +24,7 @@ public class MemberService {
             return RsData.of("F-1", "비밀번호가 일치하지 않습니다.");
         }
 
-        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username), member.getId());
+        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username), member);
     }
 
     public Member findByUsername(String username) {
@@ -32,28 +34,5 @@ public class MemberService {
     public Member findById(long id) {
         return memberRepository.findById(id);
     }
-
-        /*
-        memList.add(new Member("user1", "1234"));
-        memList.add(new Member("abc", "12345"));
-        memList.add(new Member("test", "12346"));
-        memList.add(new Member("love", "12347"));
-        memList.add(new Member("like", "12348"));
-        memList.add(new Member("giving", "12349"));
-        memList.add(new Member("thanks", "123410"));
-        memList.add(new Member("hello", "123411"));
-        memList.add(new Member("good", "123412"));
-        memList.add(new Member("peace", "123413"));
-        */
-
-        /*
-        if (!password.equals("1234")) {
-            return RsData.of("F-1", "비밀번호가 일치하지 않습니다.");
-        } else if (!username.equals("user1")) {
-            return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
-        }
-
-        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username));
-        */
 }
 

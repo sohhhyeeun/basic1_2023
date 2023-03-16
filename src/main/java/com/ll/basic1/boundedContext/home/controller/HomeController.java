7de8ae6b0ea2bde4c806e2669ba16374a9368195
sohhhyeeun <syeankm12076@gmail.com>
2023-03-16 10:15:04 +0900

    Change to RQ

--- src/main/java/com/ll/basic1/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/basic1/boundedContext/home/controller/HomeController.java
@@ -1,27 +1,33 @@
-package com.ll.basic1;
+package com.ll.basic1.boundedContext.home.controller;
 
+import com.ll.basic1.boundedContext.member.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 @Controller //개발자가 스프링부트에게 말함.(아래 있는 HomeController는 컨트롤러임.)
 public class HomeController {
     private int num;
-    private List<People> list;
-    public HomeController() {
+    private final List<People> list;
+    @Autowired
+    private final MemberService memberService;
+
+    public HomeController(MemberService memberService) {
         num = -1;
         list = new ArrayList<>();
+        this.memberService = memberService;
     }
 
     @GetMapping("/home/main")
@@ -120,6 +126,32 @@ public class HomeController {
         found.setName(name);
         found.setAge(age);
         return "%d번 사람이 수정되었습니다.".formatted(id);
+    }
+
+    @GetMapping("/home/cookie/increase")
+    @ResponseBody
+    public int showCookieIncrease(HttpServletRequest req, HttpServletResponse resp) throws IOException { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
+        int countInCookie = 0;
+
+        // 고객이 가져온 쿠폰에서 count 쿠폰을 찾고 그 쿠폰의 값을 가져온다.
+        if (req.getCookies() != null) {
+            countInCookie = Arrays.stream(req.getCookies())
+                    .filter(cookie -> cookie.getName().equals("count"))
+                    .map(Cookie::getValue)
+                    .mapToInt(Integer::parseInt)
+                    .findFirst()
+                    .orElse(0);
+        }
+
+        int newCountInCookie = countInCookie + 1;
+
+        // 고객이 가져온 count 쿠폰값에 1을 더한 쿠폰을 만들어서 고객에게 보낸다.
+        // 쉽게 말하면 브라우저(고객)에 저장되어 있는 count 쿠폰의 값을 1 증가시킨다.
+        // 이렇게 브라우저의 쿠키값을 변경하면 재방문시에 스프링부트가 다시 그 값을 받게 되어 있다.
+        resp.addCookie(new Cookie("count", newCountInCookie + ""));
+
+        // 응답 본문
+        return newCountInCookie;
     }
 }
 

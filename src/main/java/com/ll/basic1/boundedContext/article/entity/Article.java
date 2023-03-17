50566a4a21be9af35d97db629963bf6014af6adb
sohhhyeeun <syeankm12076@gmail.com>
2023-03-17 11:09:20 +0900

    Change article class

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,12 +1,20 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 public class Article {
     @Id
+    @GeneratedValue(strategy = IDENTITY) //Auto_Increment
     private long id;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title;
     private String body;
 }

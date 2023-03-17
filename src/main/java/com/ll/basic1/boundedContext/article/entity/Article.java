d7b8ca38ba41deec151e6459da797a7a8343f543
sohhhyeeun <syeankm12076@gmail.com>
2023-03-17 15:31:28 +0900

    Auto upload createDate&modifyDate

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,19 +1,34 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     @GeneratedValue(strategy = IDENTITY) //Auto_Increment
     private long id;
+    @CreatedDate
     private LocalDateTime createDate;
+    @LastModifiedDate
     private LocalDateTime modifyDate;
     private String title;
     private String body;
---
+++ src/main/java/com/ll/basic1/boundedContext/article/service/ArticleService.java
@@ -0,0 +1,26 @@
+package com.ll.basic1.boundedContext.article.service;
+
+import com.ll.basic1.boundedContext.article.entity.Article;
+import com.ll.basic1.boundedContext.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public Article write(String title, String body) {
+        Article article = Article
+                .builder()
+                .title(title)
+                .body(body)
+                .build();
+
+        articleRepository.save(article);
+
+        return article;
+    }
+}
